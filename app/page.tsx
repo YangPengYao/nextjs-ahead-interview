31c3b007c0130cab92fbbf705433314622be7a0e
paul yang <b27294301@gmail.com>
2024-12-30 20:34:40 +0800

    init: papaparse for handing csv

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import { getCSVData, get2DChartData } from "@/app/actions/get-csv-data";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const result = await getCSVData();
+  const chartData = await get2DChartData(
+    { x: "CD45-KrO", y: "SS INT LIN" },
+    result.data as Record<string, number>[]
+  );
+
+  console.log(chartData);
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Button>Test</Button>
